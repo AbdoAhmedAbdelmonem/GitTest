600d4b148338dc154f807961ab0c3c30cd6c7813
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-08-11 20:38:16 +0300

    Update hero-geometric.tsx

--- src/components/hero-geometric.tsx
+++ src/components/hero-geometric.tsx
@@ -102,6 +102,7 @@ export default function HeroGeometric({
           height={140}
           rotate={12}
           gradient="from-indigo-500/[0.15]"
+          className="left-[-10%] md:left-[-5%] top-[15%] md:top-[20%]"
         />
 
         <ElegantShape
@@ -110,6 +111,7 @@ export default function HeroGeometric({
           height={120}
           rotate={-15}
           gradient="from-rose-500/[0.15]"
+          className="right-[-5%] md:right-[0%] top-[70%] md:top-[75%]"
         />
 
         <ElegantShape
@@ -118,6 +120,7 @@ export default function HeroGeometric({
           height={80}
           rotate={-8}
           gradient="from-violet-500/[0.15]"
+          className="left-[5%] md:left-[10%] bottom-[5%] md:bottom-[10%]"
         />
 
         <ElegantShape
@@ -126,6 +129,7 @@ export default function HeroGeometric({
           height={60}
           rotate={20}
           gradient="from-amber-500/[0.15]"
+          className="right-[15%] md:right-[20%] top-[10%] md:top-[15%]"
         />
 
         <ElegantShape
@@ -134,6 +138,7 @@ export default function HeroGeometric({
           height={40}
           rotate={-25}
           gradient="from-cyan-500/[0.15]"
+          className="left-[20%] md:left-[25%] top-[5%] md:top-[10%]"
         />
       </div>
 
