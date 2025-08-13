e790f575243d1d43ee86f887f1d6894ffe3bc420
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-08-13 21:39:31 +0300

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
+import logo from '../public/images/1212-removebg-preview.png'
 import { motion, AnimatePresence } from "framer-motion"
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
@@ -43,7 +44,7 @@ export default function Navigation() {
             <motion.div whileHover={{ scale: 1.05 }} className="flex items-center gap-3">
               <div className="relative">
                   <Image
-                  src="../images/1212-removebg-preview.png"
+                  src={logo}
                   alt="Logo"
                   width={40}
                   height={40}
