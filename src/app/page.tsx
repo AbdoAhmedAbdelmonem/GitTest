a4e04f1d5f6421423921166500eb8a116379b705
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-12-06 20:31:44 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,9 +77,9 @@ const specializations = [
 
 const stats = [
   { icon: BookOpen, label: "Courses Available", value: 200, suffix: "" },
-  { icon: Award, label: "Solved Quizzes", value: 30000, suffix: "" },
+  { icon: Award, label: "Solved Quizzes", value: 36040, suffix: "" },
   { icon: BookOpenCheck, label: "Available Quizzes", value: 140, suffix: "+" },
-  { icon: ServerCrash , label: "Million Requests", value: 1.25, suffix: "M" },
+  { icon: ServerCrash , label: "Million Requests", value: 3.6, suffix: "M" },
 ]
 
 interface LevelStat {
