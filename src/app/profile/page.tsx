1ab93b63ef65b6c4f151e9376e3b9a8017690b20
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2025-12-28 15:29:16 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1006,7 +1006,7 @@ export default function ProfilePage() {
                   Your recent quiz attempts and performance
                 </CardDescription>
               </CardHeader>
-              <CardContent style={{height: "53rem"}} className="overflow-y-auto custom-scrollbar">
+              <CardContent style={{height: "64.5rem"}} className="overflow-y-auto custom-scrollbar">
                 {quizData.length > 0 ? (
                   <div className="space-y-4 flex-1">
                   {quizData.map((attempt, index) => {
@@ -1445,3 +1445,4 @@ export default function ProfilePage() {
     </div>
   )
 }
+
